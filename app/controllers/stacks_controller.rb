4581daf6e4e8680c3aeb601da068890ffeba9708
渡邉江平 <kouhei.w.56@gmail.com>
2017-09-22 10:02:22 +0900

    routes add

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,12 +1,42 @@
 class StacksController < ApplicationController
+  before_action :set_stack, only: [:edit, :show, :update, :delete]
   def index
     @stacks = Stack.all
     render json: @stacks
   end
 
-  def show
-    @stack = Stack.find(params[:id])
+  def new
+    @stack = Stack.new
     render json: @stack
+  end
+
+  def create
+    render json: @stack
+  end
+
+  def edit
+    render json: @stack
+  end
+
+  def show
+    render json: @stack
+  end
+
+  # def update
+  #   render json: @stack
+  # end
+
+  # def destroy
+  #   render json: @stack
+  # end
+
+  private
+  def stack_params
+    params.require(:stack).permit(:code, :company, :stack_price)
+  end
+
+  def set_stack
+    @stack = Stack.find(params[:id])
   end
 
 end
