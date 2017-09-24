d6ff45cf4adc1778ec6ad8b8bbca562155986f74
渡邉江平 <kouhei.w.56@gmail.com>
2017-09-24 17:32:52 +0900

    index show

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,24 +1,25 @@
 class StacksController < ApplicationController
-  before_action :set_stack, only: [:edit, :show, :update, :delete]
+  # before_action :set_stack, only: [:edit, :show, :update, :delete]
   def index
     @stacks = Stack.all
     render json: @stacks
   end
-
-  def new
-    @stack = Stack.new
-    render json: @stack
-  end
-
-  def create
-    render json: @stack
-  end
-
-  def edit
-    render json: @stack
-  end
+  #
+  # def new
+  #   @stack = Stack.new
+  #   render json: @stack
+  # end
+  #
+  # def create
+  #   render json: @stack
+  # end
+  #
+  # def edit
+  #   render json: @stack
+  # end
 
   def show
+    @stack = Stack.find(params[:id])
     render json: @stack
   end
 
@@ -30,13 +31,13 @@ class StacksController < ApplicationController
   #   render json: @stack
   # end
 
-  private
-  def stack_params
-    params.require(:stack).permit(:code, :company, :stack_price)
-  end
-
-  def set_stack
-    @stack = Stack.find(params[:id])
-  end
+  # private
+  # def stack_params
+  #   params.require(:stack).permit(:code, :company, :stack_price)
+  # end
+  #
+  # def set_stack
+  #   @stack = Stack.find(params[:id])
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :stacks, only: [:index, :new, :create, :edit, :show, :update, :destroy]
+  resources :stacks, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
